c1bbc201767aa9c5c484e44125c878af75f9eef5
euginesic <euginesichangi2@gmail.com>
2025-10-13 23:01:43 +0300

    added my app.py file

---
+++ app.py
@@ -0,0 +1,24 @@
+guess_number=input("enter your guess: ")   
+guess_attempts=3
+correct_answer=""
+
+while guess_attempts<=3:
+    
+    guess_attempts>=0
+    guess_attempts+=1
+    guess_number=input("enter your guess:")
+if   guess_number==9:
+    print("you got the right answer")
+
+else:
+    print("nope try again")      
+
+    
+
+
+
+
+
+    
+
+
